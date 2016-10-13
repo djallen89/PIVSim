c49aeeaa1a4667703e4871e2b88a27aa0503b14f
Dominick Allen <dominick.allen1989@gmail.com>
2016-10-13 14:58:10 -0400

    Initialized repo for PIVSim, which will simulate how particles behave in a vacuum.

---
+++ PIVSim/Particle.fs
@@ -0,0 +1,9 @@
+type Particle(m, v, x, y, z, q) =
+    member this.mass = m
+    member this.velocity = v
+    member this.x_coord = x
+    member this.y_coord = y
+    member this.z_coord = z
+    member this.charge = q
+
+    
--- PIVSim/Program.fs
+++ PIVSim/Program.fs
@@ -3,5 +3,5 @@
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printfn "%A" argv 
     0 // return an integer exit code
